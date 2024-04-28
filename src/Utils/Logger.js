abe1cefff537f176d389163600d6c73d028cf1b0
AceReX <ace.rexnoir@gmail.com>
2024-04-28 19:05:22 +0530

    Logger for easier pp

---
+++ src/Utils/Logger.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+export class Logger {
+  constructor() {
+    this.minLengthError = "Minimun length not satisfied!";
+    this.maxLengthError = "Maximum length reached!";
+  }
+}
