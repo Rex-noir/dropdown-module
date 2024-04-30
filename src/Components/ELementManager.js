4b4ba398420b0b9e3dc198f3278702b09a42c4c5
AceReX <ace.rexnoir@gmail.com>
2024-04-30 20:39:46 +0530

    Add byElement and byClass functions

--- src/Components/ELementManager.js
+++ src/Components/ELementManager.js
@@ -14,4 +14,18 @@ export default class ElementManger {
     }
     return document.querySelector(`#${id}`);
   }
+  /**
+   *
+   * @param {HTMLElement} element the name of element you want to search
+   * @returns the element
+   */
+  static byElement(element) {
+    return document.querySelector(element);
+  }
+  static byClass(className) {
+    if (className.startsWith(".")) {
+      return document.querySelector(className);
+    }
+    return document.querySelector(`.${className}`);
+  }
 }
