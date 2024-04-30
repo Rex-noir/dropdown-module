04f180febc6f4d41da642d48e6571fb013cd41c6
AceReX <ace.rexnoir@gmail.com>
2024-04-30 16:09:27 +0530

    Add email validator with a custom error message displaying function

--- src/Components/InputValidator.js
+++ src/Components/InputValidator.js
@@ -81,4 +81,24 @@ export default class InputValidator {
       }
     }
   }
+  /**
+   *
+   * @param {HTMLInputElement} input the input to validate
+   * @param {String} message the text to display on error
+   */
+  static setErrorMessage(input, message) {
+    input.setCustomValidity(message);
+  }
+  /**
+   *
+   * @param {HTMLInputElement} input the input to validate
+   * @returns true|false
+   */
+  static validateEmail(input) {
+    return String(input.value)
+      .toLowerCase()
+      .match(
+        /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+      );
+  }
 }
