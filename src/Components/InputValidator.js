2f057ed2601917e4398a2c74580cd0c7c47ebfd9
AceReX <ace.rexnoir@gmail.com>
2024-04-30 18:14:48 +0530

    remove most of static methods

--- src/Components/InputValidator.js
+++ src/Components/InputValidator.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-param-reassign */
 
 import { Logger } from "../Utils/Logger";
+import MyRegex from "../Utils/MyRegex";
 import ElementManger from "./ELementManager";
 import { HTMLElementBuilder } from "./HTMLElementBuilder";
 
@@ -10,15 +11,21 @@ export default class InputValidator {
    *
    * @param {HTMLInputElement} input The input element to validate
    */
-  constructor(input) {
-    if (input instanceof HTMLInputElement) {
+  constructor(input, string = "") {
+    if (this.checkInstance(input)) {
       this.input = input;
     } else {
-      throw new Error("Can't use non-input element");
+      if (string === "create") {
+        this.input = HTMLElementBuilder.create("input");
+      } else Logger.inputTypeError();
     }
   }
 
-  build() {
+  /**
+   *
+   * @returns the processed input element
+   */
+  getElement() {
     return this.input;
   }
   /**
@@ -26,7 +33,7 @@ export default class InputValidator {
    * @param {HTMLInputElement} input
    * @returns true | false
    */
-  static checkInstance(input) {
+  checkInstance(input) {
     if (input instanceof HTMLInputElement) return true;
     return false;
   }
@@ -35,70 +42,73 @@ export default class InputValidator {
    *
    * @param {HTMLInputElement} input set "required" attribute to the input
    */
-  static setRequired(input) {
-    if (this.checkInstance(input)) {
-      input.required = !input.required;
-    } else {
-      Logger.inputTypeError();
-    }
+  setRequired() {
+    this.input.required = !this.input.required;
+    return this;
   }
+
   /**
    *
-   * @param {HTMLInputElement} input input element to set autocomplete
-   * @param {Array} suggestions the arrays of suggestions to suggest
+   * @param {Array} suggestions the array of suggestions to suggest
    * You can style the elements by selecting the id, classes of the suggestions elements
    * Default selectors:
    * the whole container as ID : ${input.id}-suggestion-container
    * the list items as class : suggestion-items
    */
-  static setAutoComplete(input, suggestions) {
-    if (this.checkInstance(input)) {
-      input.autocomplete = "on";
-      if (Array.isArray(suggestions)) {
-        input.addEventListener("input", (e) => {
-          const removeDiv = ElementManger.byId(
-            `${input.id}-suggestion-container`
-          );
-          if (removeDiv) removeDiv.parentNode.removeChild(removeDiv);
-          let relevant = suggestions.filter((sugg) =>
-            sugg.toLowerCase().startsWith(input.value.toLowerCase())
-          );
-          const container = new HTMLElementBuilder("div")
-            .setId(`${input.id}-suggestion-container`)
+  setAutoComplete(suggestions) {
+    this.input.autocomplete = "on";
+    if (Array.isArray(suggestions)) {
+      this.input.addEventListener("input", (e) => {
+        const removeDiv = ElementManger.byId(
+          `${this.input.id}-suggestion-container`
+        );
+        if (removeDiv) removeDiv.parentNode.removeChild(removeDiv);
+        let relevant = suggestions.filter((sugg) =>
+          sugg.toLowerCase().startsWith(this.input.value.toLowerCase())
+        );
+        const container = new HTMLElementBuilder("div")
+          .setId(`${this.input.id}-suggestion-container`)
+          .build();
+        for (let suggestion of relevant) {
+          const items = new HTMLElementBuilder("div")
+            .setClass("suggestion-items")
+            .setText(suggestion)
+            .addEvent("click", (e) => {
+              this.input.value = items.textContent;
+            })
             .build();
-          for (let suggestion of relevant) {
-            const items = new HTMLElementBuilder("div")
-              .setClass("suggestion-items")
-              .setText(suggestion)
-              .addEvent("click", (e) => {
-                input.value = items.textContent;
-              })
-              .build();
-            container.appendChild(items);
-          }
-          input.parentNode.appendChild(container);
-        });
-      }
+          container.appendChild(items);
+        }
+        this.input.parentNode.appendChild(container);
+      });
     }
+    return this;
   }
   /**
    *
-   * @param {HTMLInputElement} input the input to validate
    * @param {String} message the text to display on error
    */
-  static setErrorMessage(input, message) {
-    input.setCustomValidity(message);
+  setErrorMessage(message) {
+    this.input.setCustomValidity(message);
+    return this;
   }
   /**
    *
-   * @param {HTMLInputElement} input the input to validate
    * @returns true|false
    */
-  static validateEmail(input) {
-    return !!String(input.value)
+  checkEmailValidiy() {
+    const validity = !!String(this.input.value)
       .toLowerCase()
-      .match(
-        /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-      );
+      .match(MyRegex.email());
+    if (validity) {
+      this.setErrorMessage("");
+      return validity;
+    }
+    return false;
   }
+  addListener(type, callbackFn) {
+    this.input.addEventListener(type, (e) => callbackFn(e));
+    return this;
+  }
+  
 }
