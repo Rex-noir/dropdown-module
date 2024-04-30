d0f944273e2edae7a1a47659fa68c4d5b0a27807
AceReX <ace.rexnoir@gmail.com>
2024-04-30 11:06:02 +0530

    finish the initial version of the autocomplete

--- src/Components/InputValidator.js
+++ src/Components/InputValidator.js
@@ -1,4 +1,9 @@
 /* eslint-disable no-param-reassign */
+
+import { Logger } from "../Utils/Logger";
+import ElementManger from "./ELementManager";
+import { HTMLElementBuilder } from "./HTMLElementBuilder";
+
 /* eslint-disable no-unused-vars */
 export default class InputValidator {
   /**
@@ -33,6 +38,43 @@ export default class InputValidator {
   static setRequired(input) {
     if (this.checkInstance(input)) {
       input.required = !input.required;
+    } else {
+      Logger.inputTypeError();
+    }
+  }
+  /**
+   *
+   * @param {HTMLInputElement} input input element to set autocomplete
+   * @param {Array} suggestions the arrays of suggestions to suggest
+   */
+  static setAutoComplete(input, suggestions) {
+    if (this.checkInstance(input)) {
+      input.autocomplete = "on";
+      if (Array.isArray(suggestions)) {
+        input.addEventListener("input", (e) => {
+          const removeDiv = ElementManger.byId(
+            `${input.id}-suggestion-container`
+          );
+          if (removeDiv) removeDiv.parentNode.removeChild(removeDiv);
+          let relevant = suggestions.filter((sugg) =>
+            sugg.toLowerCase().startsWith(input.value.toLowerCase())
+          );
+          const container = new HTMLElementBuilder("div")
+            .setId(`${input.id}-suggestion-container`)
+            .build();
+          for (let suggestion of relevant) {
+            const items = new HTMLElementBuilder("div")
+              .setClass("suggestin-items")
+              .setText(suggestion)
+              .addEvent("click", (e) => {
+                input.value = items.textContent;
+              })
+              .build();
+            container.appendChild(items);
+          }
+          input.parentNode.appendChild(container);
+        });
+      }
     }
   }
 }
